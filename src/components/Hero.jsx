5e9289c22ba7f6bf93a9f10137a131fcc80bfe33
Moustapha Mahmoud <mmm066550@gmail.com>
2022-02-21 10:15:51 +0200

    fix hero styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,10 +24,10 @@ export default function Hero() {
             </p>
             {/* NAME */}
             <div className="py-3 text-center name">
-              <Glitch>
+              <Glitch className="name-g">
                 {<RandomReveal isPlaying duration={3} characters="Moustapha" />}
               </Glitch>
-              <Glitch>
+              <Glitch className="name-g">
                 {<RandomReveal isPlaying duration={3} characters="Mahmoud" />}
               </Glitch>
             </div>
