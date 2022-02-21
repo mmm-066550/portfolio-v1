db070f946be0ad080bb215182b1610fca2a66878
Moustapha Mahmoud <mmm066550@gmail.com>
2022-02-21 09:45:27 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -41,7 +41,7 @@ export default function App() {
     //   once: false, // whether animation should happen only once - while scrolling down
     //   mirror: false, // whether elements should animate out while scrolling past them
     //   anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
-    });
+    // });
   }, []);
   const isDarkMode = DetectDarkMode.isDark;
   const [mode, setmode] = useState(
