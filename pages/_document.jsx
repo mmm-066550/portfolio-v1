c702da6f314bd6b81f99d054ee39cae0f8c8dc28
Moustapha Mahmoud <mmm066550@gmail.com>
2022-03-27 17:49:36 +0200

    Update fonts

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -4,7 +4,19 @@ import { Html, Head, NextScript, Main } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head></Head>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          rel="preload"
+          href="https://fonts.googleapis.com/css2?family=Fira+Mono:wght@400;500;700&display=optional"
+          as="font"
+        />
+      </Head>
       <body data-color="blue">
         <Main></Main>
         <NextScript></NextScript>
