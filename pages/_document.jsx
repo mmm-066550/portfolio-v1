747512ab35c057d3b19599ae490741f79e27a5be
Moustapha Mahmoud <mmm066550@gmail.com>
2022-03-27 20:40:45 +0200

    Add missed fonts ✅

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -4,18 +4,7 @@ import { Html, Head, NextScript, Main } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Fira+Mono:wght@400;500;700&display=optional"
-          rel="stylesheet"
-        />
-      </Head>
+      <Head></Head>
       <body data-color="blue">
         <Main></Main>
         <NextScript></NextScript>
