d53063c6fadce4fabc4e98ff1039a40aaadd09b7
Moustapha Mahmoud <mmm066550@gmail.com>
2022-03-27 22:46:58 +0200

    🔭📐 Set up Google Analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -17,7 +17,7 @@ function MyApp({ Component, pageProps }) {
         {`window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', '${process.env.GOOGLE_ANALYTICS_CODE}');`}
+          gtag('config', ${process.env.GOOGLE_ANALYTICS_CODE});`}
       </Script>
       <ThemeProvider defaultTheme="dark" enableSystem={false}>
         <Component {...pageProps} />
