3d11a9874490e29156ace21a07e15aa797cd4698
Moustapha Mahmoud <mmm066550@gmail.com>
2022-06-10 21:08:12 +0200

    Update index.jsx

--- components/hero/index.jsx
+++ components/hero/index.jsx
@@ -59,7 +59,7 @@ export default function Hero({ data }) {
             <div
               className={`${styles.download_resume_area} justify-content-center justify-content-md-end`}
             >
-              <Link target="__blank" href={"https://drive.google.com/file/d/18LaI4apHUl3qZj0FemgCwyaFIgGGJrrk/view?usp=sharing"}>
+              <Link target="__blank" href={data.resume}>
                 <a target="__blank" className={styles.resume_btn}>
                   <span className={styles.outer}>
                     <span className={styles.arch}></span>
